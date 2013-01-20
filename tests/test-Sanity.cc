888f6f500125a81a8f7d8a792238149ecd9b4055
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2013-01-20 00:08:06 -0800

    Fixing sanity test for 64 bits.

--- tests/test-Sanity.cc
+++ tests/test-Sanity.cc
@@ -6,8 +6,13 @@ void MainTask::Do() {
     Printer::log(M_STATUS, "Test::Sanity running.");
 
     TAssert(sizeof(off_t) == 8);
+#ifdef __x86_64__
+    TAssert(sizeof(size_t) == 8);
+    TAssert(sizeof(time_t) == 8);
+#else
     TAssert(sizeof(size_t) == 4);
     TAssert(sizeof(time_t) == 4);
+#endif
 
     Printer::log(M_STATUS, "Test::Sanity passed.");
 }
