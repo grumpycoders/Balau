db7860ff546be4c6aadaad840991e7226254fd84
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2014-07-31 23:22:26 +0200

    Seems that MSVC is also mangling symbols. Moving externs up.

--- win32/getopt/getopt.h
+++ win32/getopt/getopt.h
@@ -6,12 +6,11 @@ extern "C" {
 
 int getopt(int nargc, char ** nargv, char * ostr);
 
-#ifdef __cplusplus
-}
-#endif
-
-
 extern int opterr = 1;
 extern int optind = 1;
 extern int optopt;
 extern char * optarg;
+
+#ifdef __cplusplus
+}
+#endif
